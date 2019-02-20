e2fcc6379f23ee969383327186330c456fb45eb4
pv <pv@swooby.com>
2019-02-19 21:55:33 -0800

    Preparing to attempt to control two GUB231s...

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,65 +12,107 @@ namespace ConsoleApp1
     {
         private const int VendorId = 0x0557;
         private const int ProductId = 0x2405;
-        // GUB201: Product Version 0.1
-        //   ProductId  0x00002405
-        //   VendorId   0x00000557
-        //   Version    0x00000001 == 0.1
-        // GUB231: Product Version 49.2
-        //   ProductId  0x00002405
-        //   VendorId   0x00000557
-        //   Version    0x00003102 == 49.1
 
-        private static HidDevice _device;
+        // GUB201: Product Version 0.1
+        //   VendorId   0x00000557
+        //   ProductId  0x00002405
+        //   Version    0x00000001 (1) == 0.1
+        // GUB231: Product Version 49.2
+        //   VendorId   0x00000557
+        //   ProductId  0x00002405
+        //   Version    0x00003102 (12546) == 49.2
+
+        private string DeviceModel;
+
+        private static string _deviceModel;
+        private static HidDevice _device1;
+        private static HidDevice _device2;
         private static bool _attached;
+
+        static void Prompt(string message)
+        {
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+        static void Exit(string message)
+        {
+            Prompt(message);
+            Environment.Exit(1);
+        }
 
         static void Main(string[] args)
         {
             var devices = HidDevices.Enumerate(VendorId, ProductId);
-            _device = devices.FirstOrDefault();
-            if (_device == null)
+            if (devices.Count() == 0)
             {
-                Console.WriteLine("Could not find GUB201 contoller; Press any key to exit...");
-                Console.ReadKey();
+                Exit("Could not find USB A/B switch contoller; Press any key to exit...");
                 return;
             }
 
-            _device.Inserted += DeviceAttachedHandler;
-            _device.Removed += DeviceRemovedHandler;
+            foreach(var device in devices)
+            {
+                switch (device.Attributes.Version)
+                {
+                    case 0x00003102:
+                        if (_deviceModel == null)
+                        {
+                            _deviceModel = "GUB231";
+                        }
+                        if (_device1 == null)
+                        {
+                            _device1 = device;
+                        }
+                        else if (_device2 == null)
+                        {
+                            _device2 = device;
+                        }
+                        break;
+                    case 0x00000001:
+                        // GUB201
+                        break;
+                }
+            }
 
-            _device.MonitorDeviceEvents = true;
+            if (_device1 == null)
+            {
+                Exit("Could not find USB A/B switch contoller; Press any key to exit...");
+                return;
+            }
 
-            Console.WriteLine("GUB201 contoller attached; Press any key to switch...");
-            Console.ReadKey();
+            _device1.Inserted += DeviceAttachedHandler;
+            _device1.Removed += DeviceRemovedHandler;
+            _device1.MonitorDeviceEvents = true;
+
+            Prompt($"{_deviceModel} contoller attached; Press any key to switch...");
+
             Switch();
 
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+            Exit("Press any key to exit...");
         }
 
         private static void DeviceAttachedHandler()
         {
             _attached = true;
-            //Console.WriteLine("GUB201 contoller attached.");
+            //Console.WriteLine($"{_deviceModel} contoller attached.");
         }
 
         private static void DeviceRemovedHandler()
         {
             _attached = false;
-            Console.WriteLine("GUB201 contoller detached.");
+            Console.WriteLine($"{_deviceModel} contoller detached.");
         }
 
         private static void Switch()
         {
-            if (_device == null || !_attached)
+            if (_device1 == null || !_attached)
             {
                 return;
             }
 
-            _device.OpenDevice();
+            _device1.OpenDevice();
             //_device.WriteReport(new HidReport(2, new HidDeviceData(new byte[] { 0x02, 0x23 }, HidDeviceData.ReadStatus.Success))); // GUB201
-            _device.WriteReport(new HidReport(2, new HidDeviceData(new byte[] { 0x02, 0x11 }, HidDeviceData.ReadStatus.Success))); // GUB231
-            _device.CloseDevice();
+            _device1.WriteReport(new HidReport(2, new HidDeviceData(new byte[] { 0x02, 0x11 }, HidDeviceData.ReadStatus.Success))); // GUB231
+            _device1.CloseDevice();
         }
     }
 }
